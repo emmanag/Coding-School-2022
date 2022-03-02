f3cf6da7660299beafaa35327fa671dd6d32a805
Manos Anagnostopoulos <manos.anag88@gmail.com>
2022-03-02 14:37:33 +0200

    2 first examples

---
+++ Session_2b/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Session_2b_Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("Hello Manos!");
+
+            //2
+            int x = 6;
+            int y = 3;
+            int sum = x + y;
+            int div = x / y;
+            Console.WriteLine(sum);
+            Console.WriteLine(div);
+
+            //3
+            Console.WriteLine();
+        }
+    }
+}
