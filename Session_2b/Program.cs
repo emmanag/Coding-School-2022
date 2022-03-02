34e07e735da1b946a09fb7fae29a66249a0ace50
Manos Anagnostopoulos <manos.anag88@gmail.com>
2022-03-02 16:05:12 +0200

    new changes

--- Session_2b/Program.cs
+++ Session_2b/Program.cs
@@ -16,8 +16,8 @@
             Console.WriteLine(div);
 
             //3
-            Console.WriteLine((-1 + 5) * 6);
-            Console.WriteLine(14 + ((-3 * 6) / 7));
+            Console.WriteLine(-1 + 5 * 6);
+            Console.WriteLine(14 + (-3 * 6) / 7);
             Console.WriteLine(2 + (13 / 6) * 6 * Math.Sqrt(7));
             Console.WriteLine(38*(5 % 7));
             Console.WriteLine(((Math.Pow(6, 4) + Math.Pow(5, 7) / (9 % 4))));
