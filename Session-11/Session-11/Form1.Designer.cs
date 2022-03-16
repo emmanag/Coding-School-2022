77cc43b14e4f567af37c968ba02a6085aff186c0
Manos Anagnostopoulos <manos.anag88@gmail.com>
2022-03-16 15:23:08 +0200

    commit branch test

--- Session-11/Session-11/Form1.Designer.cs
+++ Session-11/Session-11/Form1.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(164, 116);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(63, 13);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "labelControl1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
     }
 }
 
