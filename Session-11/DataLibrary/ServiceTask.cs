2d15f8fd7f6ba539dbded3bb9806929f990e4e09
rkeytech <rkeytech@protonmail.com>
2022-03-16 19:13:01 +0200

    Added DataItem class for ID

---
+++ Session-11/DataLibrary/ServiceTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    internal class ServiceTask
+    {
+        public Guid ID { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public double Hours { get; set; }
+
+        public ServiceTask()
+        {
+
+        }
+    }
+}
