070702c5fa42be25e5b6be759ace5990b1c692f4
Manos Anagnostopoulos <manos.anag88@gmail.com>
2022-03-17 16:51:26 +0200

    ServiceTask, Transaction, TransactionLine  Handlers

---
+++ Session-11/DataLibrary/TransactionLineHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class TransactionLineHandler : ActionHandler
+    {
+        public TransactionLineHandler()
+        {
+
+        }
+
+        public override object Create()
+        {
+            return new TransactionLine();
+        }
+
+        public override void Delete<TransactionLine>(TransactionLine transactionLine, List<TransactionLine> transactionLines)
+        {
+            transactionLines.Remove(transactionLine);
+        }
+    }
+}
