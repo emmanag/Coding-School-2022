b7f6cfff3755e3aa8609736ed18615a9039da0d8
siderism <michaelsid4@gmail.com>
2022-03-17 16:24:44 +0200

    recreated files

---
+++ Session-11/DataLibrary/ServiceTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class ServiceTask : Item
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Hours { get; set; }
+
+        public ServiceTask()
+        {
+
+        }
+    }
+}
