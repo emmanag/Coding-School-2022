830dc954e5ae07bc8aa2a5f23042b753623af2c4
rkeytech <rkeytech@protonmail.com>
2022-03-17 17:12:59 +0200

    Added new Handler Classes
    * ManagerHandler Class
    * EngineerHandler Class

---
+++ Session-11/DataLibrary/EngineerHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    internal class EngineerHandler : ActionHandler
+    {
+        public EngineerHandler()
+        {
+
+        }
+
+        public override object Create()
+        {
+            return new Engineer();
+        }
+
+        public override void Delete<Engineer>(Engineer obj, List<Engineer> list)
+        {
+            list.Remove(obj);
+        }
+    }
+}
---
+++ Session-11/DataLibrary/ManagerHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class ManagerHandler : ActionHandler
+    {
+        public ManagerHandler()
+        {
+
+        }
+
+        public override object Create()
+        {
+            return new Manager();
+        }
+
+        public override void Delete<Manager>(Manager obj, List<Manager> list)
+        {
+            list.Remove(obj);
+        }
+    }
+}
