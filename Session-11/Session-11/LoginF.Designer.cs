75dac7a1c8fc67e2715cc13055a43279542afe13
Manos Anagnostopoulos <manos.anag88@gmail.com>
2022-03-17 15:19:14 +0200

    Login creation form

---
+++ Session-11/Session-11/LoginF.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Session_11
+{
+    partial class LoginF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.Btnenter = new DevExpress.XtraEditors.SimpleButton();
+            this.Btnexit = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(170, 116);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(48, 13);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "Username";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(170, 164);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(46, 13);
+            this.labelControl2.TabIndex = 1;
+            this.labelControl2.Text = "Password";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(245, 113);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(222, 20);
+            this.textEdit1.TabIndex = 2;
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(245, 161);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(222, 20);
+            this.textEdit2.TabIndex = 3;
+            // 
+            // Btnenter
+            // 
+            this.Btnenter.Location = new System.Drawing.Point(245, 268);
+            this.Btnenter.Name = "Btnenter";
+            this.Btnenter.Size = new System.Drawing.Size(75, 23);
+            this.Btnenter.TabIndex = 4;
+            this.Btnenter.Text = "Enter";
+            // 
+            // Btnexit
+            // 
+            this.Btnexit.Location = new System.Drawing.Point(392, 268);
+            this.Btnexit.Name = "Btnexit";
+            this.Btnexit.Size = new System.Drawing.Size(75, 23);
+            this.Btnexit.TabIndex = 5;
+            this.Btnexit.Text = "Exit";
+            // 
+            // LoginF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 450);
+            this.Controls.Add(this.Btnexit);
+            this.Controls.Add(this.Btnenter);
+            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Name = "LoginF";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.SimpleButton Btnenter;
+        private DevExpress.XtraEditors.SimpleButton Btnexit;
+    }
+}
