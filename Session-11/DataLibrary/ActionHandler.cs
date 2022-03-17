da5bfc87bd417f0586f89147d00654e6739ee42f
siderism <michaelsid4@gmail.com>
2022-03-17 15:08:30 +0200

    create actionHandler, carHandler

---
+++ Session-11/DataLibrary/ActionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class ActionHandler
+    {
+        public ActionHandler()
+        {
+
+        }
+
+        public virtual object Create()
+        {
+            return null;
+        }
+
+        public virtual void Delete<T>(T obj, List<T> list)
+        {
+            list.Remove(obj);
+        }
+    }
+}
