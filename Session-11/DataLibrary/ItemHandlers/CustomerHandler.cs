55af4a1e7da1fdfb538e2ad9ef0d72b3bd75dbe5
siderism <michaelsid4@gmail.com>
2022-03-18 14:29:34 +0200

    adde CustomerHandler

---
+++ Session-11/DataLibrary/ItemHandlers/CustomerHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.ItemHandlers
+{
+    public class CustomerHandler : ActionHandler
+    {
+        public CustomerHandler()
+        {
+
+        }
+
+        public override object Create()
+        {
+            return new Customer();
+        }
+
+        public override void Delete<Customer>(Customer customer, List<Customer> customers)
+        {
+            customers.Remove(customer);
+        }
+    }
+}
