c0744df8450001bf23bf9497194de9fc341afcc0
rkeytech <rkeytech@protonmail.com>
2022-03-18 18:04:38 +0200

    Changed CarHandler to break the inheritance

---
+++ Session-11/DataLibrary/ItemHandlers/CarServiceHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.ItemHandlers
+{
+    internal class CarServiceHandler
+    {
+        public CarServiceHandler()
+        {
+
+        }
+
+        public decimal GetMaxDayWorkload()
+        {
+
+        }
+
+        public decimal GetReservedHours()
+        {
+
+        }
+    }
+}
