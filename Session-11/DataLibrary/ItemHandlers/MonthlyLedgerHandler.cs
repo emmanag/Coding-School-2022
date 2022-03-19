196ff9625f2974f3e7c599780d22ae9f1e34585d
siderism <michaelsid4@gmail.com>
2022-03-19 16:53:35 +0200

    created handler

---
+++ Session-11/DataLibrary/ItemHandlers/MonthlyLedgerHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.ItemHandlers
+{
+    public class MonthlyLedgerHandler
+    {
+        public MonthlyLedgerHandler()
+        {
+
+        }
+
+        //public decimal GetMonthlyIncome(MonthsEnum month, string year, CarService carService)
+        //{
+        //    return carService.Transactions.FindAll(t => t.Date.Month == month && t.Date.Year == month)
+        //}
+    }
+}
