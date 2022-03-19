7dfd6cd8cffa1badc675df8a25287bddde3d18a8
siderism <michaelsid4@gmail.com>
2022-03-19 17:36:36 +0200

    update MontlyLedger props

--- Session-11/DataLibrary/Enums.cs
+++ Session-11/DataLibrary/Enums.cs
@@ -6,21 +6,6 @@ using System.Threading.Tasks;
 
 namespace DataLibrary
 {
-    public enum MonthsEnum
-    {
-        January,
-        February,
-        March,
-        April,
-        May,
-        June,
-        July,
-        August,
-        September,
-        October,
-        November,
-        December
-    }
     public enum StatusEnum
     {
         Free,
