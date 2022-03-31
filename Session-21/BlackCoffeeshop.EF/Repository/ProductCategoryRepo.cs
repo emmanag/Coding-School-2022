b09fcdfaa9a74a450b7ff00e684aa1283b033caf
Jimbos <tr9l9l9@hotmail.com>
2022-03-31 17:15:49 +0300

    Added ProductCategoryRepo.

--- Session-21/BlackCoffeeshop.EF/Repository/ProductCategoryRepo.cs
+++ Session-21/BlackCoffeeshop.EF/Repository/ProductCategoryRepo.cs
@@ -1,11 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BlackCoffeeshop.EF.Context;
+using BlackCoffeeshop.EF.Repository;
+using BlackCoffeeshop.Model;
 
 namespace BlackCoffeeshop.EF.Configuration
 {
-    public class ProductCategoryRepo {
+    public class ProductCategoryRepo : IEntityRepo<ProductCategory> {
+        public async Task Create(ProductCategory entity) {
+            using var context = new ApplicationContext();
+            context.ProductCategories.Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id) {
+            using var context = new ApplicationContext();
+            var foundProdCat = context.ProductCategories.SingleOrDefault(prodCat => prodCat.ID == id);
+            if (foundProdCat is null)
+                return;
+
+            context.ProductCategories.Remove(foundProdCat);
+            await context.SaveChangesAsync();
+        }
+
+        public List<ProductCategory> GetAll() {
+            using var context = new ApplicationContext();
+            return context.ProductCategories.ToList();
+        }
+
+        public ProductCategory? GetById(int id) {
+            using var context = new ApplicationContext();
+            return context.ProductCategories.Where(prodCat => prodCat.ID == id).SingleOrDefault();
+        }
+
+        public async Task Update(int id, ProductCategory entity) {
+            using var context = new ApplicationContext();
+            var foundProductCat = context.ProductCategories.SingleOrDefault(prodCat => prodCat.ID == id);
+            if (foundProductCat is null)
+                return;
+
+            foundProductCat.Code = entity.Code;
+            foundProductCat.Description = entity.Description;
+            foundProductCat.ProductType = entity.ProductType;
+            foundProductCat.Transaction = entity.Transaction;
+
+            await context.SaveChangesAsync();
+        }
     }
 }
