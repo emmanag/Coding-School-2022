b92f87dd6cac24bc27ac038eded6b737a14c18f3
Jimbos <tr9l9l9@hotmail.com>
2022-03-31 17:23:00 +0300

    Added TransactionLineRepo

--- Session-21/BlackCoffeeshop.EF/Repository/TransactionLineRepo.cs
+++ Session-21/BlackCoffeeshop.EF/Repository/TransactionLineRepo.cs
@@ -1,11 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BlackCoffeeshop.EF.Context;
+using BlackCoffeeshop.EF.Repository;
+using BlackCoffeeshop.Model;
 
-namespace BlackCoffeeshop.EF.Configuration
-{
-    public class TransactionLineRepo {
+namespace BlackCoffeeshop.EF.Configuration {
+    public class TransactionLineRepo : IEntityRepo<TransactionLine> {
+        public async Task Create(TransactionLine entity) {
+            using var context = new ApplicationContext();
+            context.TransactionLines.Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id) {
+            using var context = new ApplicationContext();
+            var foundTransLine = context.TransactionLines.SingleOrDefault(transLine => transLine.ID == id);
+            if (foundTransLine is null)
+                return;
+
+            context.TransactionLines.Remove(foundTransLine);
+            await context.SaveChangesAsync();
+        }
+
+        public List<TransactionLine> GetAll() {
+            using var context = new ApplicationContext();
+            return context.TransactionLines.ToList();
+        }
+
+        public TransactionLine? GetById(int id) {
+            using var context = new ApplicationContext();
+            return context.TransactionLines.Where(transline => transline.ID == id).SingleOrDefault(); ;
+        }
+
+        public async Task Update(int id, TransactionLine entity) {
+            using var context = new ApplicationContext();
+            var foundTransLine = context.TransactionLines.SingleOrDefault(transLine => transLine.ID == id);
+            if (foundTransLine is null)
+                return;
+
+            foundTransLine.ProductID = entity.ProductID;
+            foundTransLine.TransactionID = entity.TransactionID;
+            foundTransLine.Quantity = entity.Quantity;
+            foundTransLine.ProductDes = entity.ProductDes;
+            foundTransLine.Price = entity.Price;
+            foundTransLine.DiscountPerCent = entity.DiscountPerCent;
+            foundTransLine.Discount = entity.Discount;
+            foundTransLine.DisplayPrice = entity.DisplayPrice;
+            foundTransLine.TotalPrice = entity.TotalPrice;
+            foundTransLine.TotalCost = entity.TotalCost;
+
+            await context.SaveChangesAsync();
+        }
     }
 }
