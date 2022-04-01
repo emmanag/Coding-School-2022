e245d4ffa2c9a0a48f0be324c715f58b496ea88b
ChrisDomp <dompazis@auth.gr>
2022-04-01 15:28:08 +0300

    Added mock repo for employee

---
+++ Session-21/BlackCoffeeshop.EF/Repository/MockEmployeeRepo.cs
@@ -0,0 +1,61 @@
+﻿using BlackCoffeeshop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackCoffeeshop.EF.Repository;
+
+public class MockEmployeeRepo : IEntityRepo<Employee>
+{
+    private readonly List<Employee> _employees;
+    public MockEmployeeRepo()
+    {
+        _employees = new List<Employee>
+        {
+            new Employee() {
+                EmployeeType = EmployeeType.Cashier,
+                Name = "Takis",
+                Surname = "Papadakis",
+                SalaryPerMonth = 900
+            }
+        };
+    }
+    /// <inheritdoc />
+    public async Task Create(Employee entity)
+    {
+        _employees.Add(entity);
+    }
+
+    public async Task Delete(int id)
+    {
+        _employees.Remove(GetById(id));
+    }
+
+    public List<Employee> GetAll()
+    {
+        return _employees;
+    }
+
+    public Employee? GetById(int id)
+    {
+        return _employees.SingleOrDefault(Employee => Employee.ID == id);
+    }
+
+    public async Task Update(int id, Employee entity)
+    {
+        var foundEmployee = _employees.SingleOrDefault(Employee => Employee.ID == id);
+
+        if (foundEmployee is null)
+            throw new KeyNotFoundException($"Given id '{id}' was not found");
+
+        foundEmployee.Name = entity.Name;
+        foundEmployee.Surname = entity.Surname;
+        foundEmployee.SalaryPerMonth = entity.SalaryPerMonth;
+        foundEmployee.EmployeeType = entity.EmployeeType;
+
+    }
+
+
+}
