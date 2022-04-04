e2b1cd30fcdf8569b508bde1891039effdb30996
Jimbos <tr9l9l9@hotmail.com>
2022-04-04 15:19:48 +0300

    Added first draft of ledger

---
+++ Session-21/BlackCoffeeShop/Models/LedgerViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BlackCoffeeShop.Web.Models {
+    public class LedgerViewModel {
+        private decimal _stockCost;
+        private decimal _rentCost;
+        private decimal _fixedExpenses;
+        LedgerViewModel() {
+            _stockCost = 0;
+            _rentCost = 3000;
+            _fixedExpenses = _rentCost;
+        }
+       
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public decimal Income { get; set; }
+        public decimal Expenses => _fixedExpenses + _stockCost;
+        public decimal Total => Income - Expenses;
+    }
+}
+/*Year, Month, Income, Expenses, Total*/
